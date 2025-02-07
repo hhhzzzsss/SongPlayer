19e9178cd8c4692c8fc75de4edb452cdb5c38a69
hhhzzzsss <hyzhou42@gmail.com>
2025-02-07 04:27:11 -0600

    Return player to stage position when survival playing finishes

--- src/main/java/com/github/hhhzzzsss/songplayer/playing/SongHandler.java
+++ src/main/java/com/github/hhhzzzsss/songplayer/playing/SongHandler.java
@@ -371,7 +371,7 @@ public class SongHandler {
                         stage.checkSurvivalBuildStatus(currentSong);
                     } catch (Stage.NotEnoughInstrumentsException e) {
                         SongPlayer.addChatMessage("§6Stopped because stage is missing instruments required for song.");
-                        reset();
+                        restoreStateAndReset();
                         return;
                     }
                 }
