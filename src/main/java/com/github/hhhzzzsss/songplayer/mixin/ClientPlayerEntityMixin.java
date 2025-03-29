60512beb40e67e8d225f639e4913ff2362e81df9
ZacSharp <68165024+ZacSharp@users.noreply.github.com>
2025-03-29 04:05:14 +0100

    Avoid `@Redirect` conflict by using `@ModifyExpressionValue`
    This prevents a redirect conflict with Baritone

--- src/main/java/com/github/hhhzzzsss/songplayer/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/github/hhhzzzsss/songplayer/mixin/ClientPlayerEntityMixin.java
@@ -1,21 +1,20 @@
 package com.github.hhhzzzsss.songplayer.mixin;
 
 import com.github.hhhzzzsss.songplayer.playing.SongHandler;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.player.PlayerAbilities;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin {
-    @Redirect(method = "tickMovement()V", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerAbilities;allowFlying:Z", opcode = Opcodes.GETFIELD))
-    private boolean getAllowFlying(PlayerAbilities playerAbilities) {
+    @ModifyExpressionValue(method = "tickMovement()V", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerAbilities;allowFlying:Z", opcode = Opcodes.GETFIELD))
+    private boolean getAllowFlying(boolean allowFlying) {
         if (!SongHandler.getInstance().isIdle()) {
             return true;
         } else {
-            return playerAbilities.allowFlying;
+            return allowFlying;
         }
     }
 }
